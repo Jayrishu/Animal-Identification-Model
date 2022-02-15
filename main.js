773298e0d7ea5cc73662761064c2068781ac3bc4
Jayrishu <91966368+Jayrishu@users.noreply.github.com>
2022-02-15 13:41:16 +0400

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function start(){
-    navigator.mediaDevices.getUserMedia({audio:true});
-    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/AbLRfA0QE/model.json", modelReady);
-
-}
-function modelReady(){
-    console.log("Model is ready");
-    classifier.classify(gotResults);
-}
